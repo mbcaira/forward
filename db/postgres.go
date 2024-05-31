03568481cf455a4d1aa315e77ad3d77810311427
Michael Caira <mbcaira@gmail.com>
2024-05-30 23:44:11 -0400

    Add skeleton for db connection

---
+++ db/postgres.go
@@ -0,0 +1,13 @@
+package db
+
+func QueryUrlEntries(longUrl string) (result string, urlFound bool, err error) {
+
+}
+
+func WriteUrlEntry(uuid string, shortUrl string, longUrl string) (id string, err error) {
+
+}
+
+func ReadDb(query string) (result []string, err error) {}
+
+func WriteDb() (id string, err error) {}
--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	router := gin.Default()
 	router.POST("api/v1/data/shorten", api.Shorten)
-	router.GET("api/v1/shortUrl", api.ShortUrl)
+	router.GET("/:shortUrl", api.ShortUrl)
 
 	router.Run()
 }
